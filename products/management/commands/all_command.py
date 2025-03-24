42b2bbdaf23c2154fa82078ecc0593f544a5d0a5
ArtemShmaraev <74972298+ArtemShmaraev@users.noreply.github.com>
2025-03-24 16:24:31 +0300

    all_command

---
+++ products/management/commands/all_command.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = 'My custom command'
+
+    def handle(self, *args, **options):
+        # Выполнение команды python manage.py makemigrations
+        call_command('flush')
+
+        call_command('makemigrations')
+
+        # Выполнение команды python manage.py migrate
+        call_command('migrate')
+
+        # Выполнение команды python manage.py add_cat
+        call_command('add_cat')
+
+        # Выполнение команды python manage.py add_size_table
+        call_command('add_size_table')
+
+        # Выполнение команды python manage.py add_sg
+        call_command('add_sg')
+
+        print('All commands executed successfully.')

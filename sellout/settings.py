f9a4d4d27da49c206cc51e16fc9dade49930c781
Ruslan Shaykhutdinov <wiwkw@mail.ru>
2025-04-06 18:32:51 +0300

    new wl

--- sellout/settings.py
+++ sellout/settings.py
@@ -175,7 +175,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,

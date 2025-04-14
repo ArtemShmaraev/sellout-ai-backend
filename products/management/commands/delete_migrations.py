42c139acc6fba47275221e48ae77e0e564085913
Ruslan Shaykhutdinov <wiwkw@mail.ru>
2025-04-14 22:51:34 +0300

    new command add_poizon blyat

--- products/management/commands/delete_migrations.py
+++ products/management/commands/delete_migrations.py
@@ -1,6 +1,5 @@
 import os
 from django.core.management.base import BaseCommand
-from django.core.management import call_command
 
 class Command(BaseCommand):
     help = 'My custom command'
@@ -9,13 +8,16 @@ class Command(BaseCommand):
         # Delete all existing migration files
         self.delete_migration_files()
 
-        # Execute the makemigrations command
-        call_command('makemigrations')
-
-        # Execute the migrate command
-        call_command('migrate')
-
     def delete_migration_files(self):
-        # Run the shell command to delete migration files
-        os.system('find . -path "*/migrations/*.py" -not -name "__init__.py" -delete')
+        # Get the root directory of your Django project
+        project_root = os.getcwd()
+
+        # Traverse all apps in the project
+        for root, dirs, files in os.walk(project_root):
+            for file in files:
+                # Check if the file is a migration file
+                if file.endswith('.py') and 'migrations' in root and "__init__" not in file:
+                    # Delete the migration file
+                    os.remove(os.path.join(root, file))
+
         self.stdout.write(self.style.SUCCESS('Deleted all migration files'))

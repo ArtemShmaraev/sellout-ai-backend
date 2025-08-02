dee8f0f8bebf4956f79f0f85a84f7f24ca5df21c
Ruslan Shaykhutdinov <wiwkw@mail.ru>
2025-08-02 14:54:51 +0300

    Я старался

--- shipping/views.py
+++ shipping/views.py
@@ -93,7 +93,7 @@ class MinPriceForSizeView(APIView):
                     d['is_sale'] = prices['is_sale']
                     d['is_return'] = prices['is_return']
                     d['size'] = list(set(prices["size_sellout"]))
-                    d['view_size'] = size
+                    d['view_size'] = size.replace("INT", "")
                 min_prices_by_size[size] = d
                 s.append(d)
 

7bdc644bcae3a4d51c393cb05b9487fbeff267dd
ArtemShmaraev <74972298+ArtemShmaraev@users.noreply.github.com>
2026-02-18 21:25:38 +0300

    update ps

--- sellout/settings.py
+++ sellout/settings.py
@@ -249,15 +249,15 @@ SIMPLE_JWT = {
 
 
 #
-# HOST = "127.0.0.1"
-# BACKEND_HOST = "127.0.0.1:8000"
-# FRONTEND_HOST = "localhost:3000"
+HOST = "127.0.0.1"
+BACKEND_HOST = "127.0.0.1:8000"
+FRONTEND_HOST = "localhost:3000"
 
-ELASTIC_HOST = "62.84.117.234"
+ELASTIC_HOST = "51.250.91.44"
 # #
-HOST = "sellout.su"
-BACKEND_HOST = "sellout.su"
-FRONTEND_HOST = "sellout.su"
+# HOST = "sellout.su"
+# BACKEND_HOST = "sellout.su"
+# FRONTEND_HOST = "sellout.su"
 
 DATABASES = {
     'default': {
